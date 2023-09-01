43dbaad43fd29532c944c379aebfcb0ddc8fc08f
parrotace <parrotace@outlook.com>
2023-09-01 13:33:57 +0800

    modify

--- php/unsafe-functions.php
+++ php/unsafe-functions.php
@@ -1,21 +1,9 @@
+<?php
+
 // ruleid:unsafe-exec
 exec("ls");
 
-assert('jklj')
+assert('jklj');
 
 preg_replace('sdsaf')
 
-// ruleid:unsafe-exec
-exec(some_var);
-
-// ruleid:unsafe-exec
-exec (foo);
-
-// ruleid:unsafe-exec
-exec (
-  bar
-);
-
-// exec(foo)
-
-console.log("exec(bar)")
